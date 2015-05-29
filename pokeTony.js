a98961c0a496441cd813a7960d7772c87e7da682
Antonelli Briceno <tony@carrotcreative.com>
2015-05-29 16:50:41 -0400

    poke interval randomizer

--- pokeTony.js
+++ pokeTony.js
@@ -1,19 +1,25 @@
 // Poke Tony
 // v0.0.1
 
-var whoAmIPoking = "Tony"
-var howOften = 30000 // 30 seconds
+(function poker() {
+  var whoAmIPoking = "Bob"
+  var minHowOften = 20000 // 20 seconds
+  var maxHowOften = 40000 // 40 seconds
+  var howOften = Math.round(Math.random() * (maxHowOften - minHowOften)) + minHowOften;
 
-setInterval(function(){ 
-  var poke = document.getElementById("poke_live_new").querySelectorAll("[rel=async-post]")
-  if(poke.length > 0) {
-    if(poke[0].getAttribute("title") != "Remove") {
-      poke[0].click();
-      console.log(whoAmIPoking + " poked!");
+  setTimeout(function() {
+    var poke = document.getElementById("poke_live_new").querySelectorAll("[rel=async-post]");
+    if(poke.length > 0) {
+      if(poke[0].getAttribute("title") != "Remove") {
+        poke[0].click();
+        console.log(whoAmIPoking + " poked!");
+      } else {
+        console.log("No pokes to poke back.");
+      }
     } else {
       console.log("No pokes to poke back.");
     }
-  } else {
-    console.log("No pokes to poke back.");
-  }
-}, howOften); 
+    poker();
+  }, howOften);
+
+}());
